98f532a576e98ad57856a1507a5b54c98a619733
DipikaHalunde03 <dh071844@gmail.com>
2024-10-05 17:46:52 +0530

    wd201

--- cli-demo/script.js
+++ cli-demo/script.js
@@ -34,3 +34,155 @@ if (operation === 'add') {
 } else {
   console.log('Unknown operation');
 }
+
+// // const http = require('http');
+// // const fs = require('fs');
+// // const url = require('url');
+// // const path = require('path');
+
+// // const args = process.argv.slice(2);
+// // const port = args[0] || 5000;
+
+// // const server = http.createServer((req, res) => {
+// //   const parsedUrl = url.parse(req.url);
+// //   const pathname = parsedUrl.pathname;
+
+// //   if (pathname === '/') {
+// //     fs.readFile(path.join(__dirname, 'home.html'), (err, data) => {
+// //       if (err) {
+// //         res.writeHead(500, { 'Content-Type': 'text/plain' });
+// //         res.write('Error loading home page');
+// //         res.end();
+// //       } else {
+// //         res.writeHead(200, { 'Content-Type': 'text/html' });
+// //         res.write(data);
+// //         res.end();
+// //       }
+// //     });
+// //   } else if (pathname === '/project') {
+// //     fs.readFile(path.join(__dirname, 'project.html'), (err, data) => {
+// //       if (err) {
+// //         res.writeHead(500, { 'Content-Type': 'text/plain' });
+// //         res.write('Error loading project page');
+// //         res.end();
+// //       } else {
+// //         res.writeHead(200, { 'Content-Type': 'text/html' });
+// //         res.write(data);
+// //         res.end();
+// //       }
+// //     });
+// //   } else if (pathname === '/registration') {
+// //     fs.readFile(path.join(__dirname, 'registration.html'), (err, data) => {
+// //       if (err) {
+// //         res.writeHead(500, { 'Content-Type': 'text/plain' });
+// //         res.write('Error loading registration page');
+// //         res.end();
+// //       } else {
+// //         res.writeHead(200, { 'Content-Type': 'text/html' });
+// //         res.write(data);
+// //         res.end();
+// //       }
+// //     });
+// //   } else {
+// //     res.writeHead(404, { 'Content-Type': 'text/plain' });
+// //     res.write('Page not found');
+// //     res.end();
+// //   }
+// // });
+
+// // server.listen(port, () => {
+// //   console.log(`Server running on port ${port}`);
+// // });
+
+
+
+// /*
+// const fs = require("fs");
+
+// fs.writeFile(
+//   "sample.txt",
+//   "Hello World. Welcome to Node.js File System module.",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File created!");
+//   }
+// );
+
+// fs.appendFile("sample.txt", " This is my updated content", (err) => {
+//   if (err) throw err;
+//   console.log("File updated!");
+// });
+// fs.readFile("sample.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
+
+// fs.rename("sample.txt", "test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File name updated!");
+// });
+
+// fs.unlink("test.txt", (err) => {
+//   if (err) throw err;
+//   console.log("File test.txt deleted successfully!");
+// });
+
+// */
+
+// // const http=require("http");
+// // const fs=require("fs");
+
+// // const server=http.createServer((req,res) =>
+// // {
+// //   // fs.readFile("sample.txt" ,(err,data)=>
+// //   // {
+// //   //   res.end(data);
+// //   // })
+
+// //   const stream=fs.createReadStream("sample.txt");
+// //   stream.pipe();
+  
+
+// // });
+// // server.listen(3000);
+
+
+// const http = require("http");
+// const fs = require("fs");
+
+// let homeContent = "";
+// let projectContent = "";
+
+// fs.readFile("home.html", (err, home) => {
+//   if (err) {
+//     throw err;
+//   }
+//   homeContent = home;
+// });
+
+// fs.readFile("project.html", (err, project) => {
+//   if (err) {
+//     throw err;
+//   }
+//   projectContent = project;
+// });
+
+
+// http
+//   .createServer((request, response) => {
+//     let url = request.url;
+//     response.writeHeader(200, { "Content-Type": "text/html" });
+//     switch (url) {
+//       case "/project":
+//         response.write(projectContent);
+//         response.end();
+//         break;
+//       default:
+//         response.write(homeContent);
+//         response.end();
+//         break;
+//     }
+//   })
+//   .listen(3000);
+
+  
--- http-server/index.js
+++ http-server/index.js
@@ -1,121 +1,18 @@
-// const http = require('http');
-// const fs = require('fs');
-// const url = require('url');
-// const path = require('path');
-
-// const args = process.argv.slice(2);
-// const port = args[0] || 5000;
-
-// const server = http.createServer((req, res) => {
-//   const parsedUrl = url.parse(req.url);
-//   const pathname = parsedUrl.pathname;
-
-//   if (pathname === '/') {
-//     fs.readFile(path.join(__dirname, 'home.html'), (err, data) => {
-//       if (err) {
-//         res.writeHead(500, { 'Content-Type': 'text/plain' });
-//         res.write('Error loading home page');
-//         res.end();
-//       } else {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(data);
-//         res.end();
-//       }
-//     });
-//   } else if (pathname === '/project') {
-//     fs.readFile(path.join(__dirname, 'project.html'), (err, data) => {
-//       if (err) {
-//         res.writeHead(500, { 'Content-Type': 'text/plain' });
-//         res.write('Error loading project page');
-//         res.end();
-//       } else {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(data);
-//         res.end();
-//       }
-//     });
-//   } else if (pathname === '/registration') {
-//     fs.readFile(path.join(__dirname, 'registration.html'), (err, data) => {
-//       if (err) {
-//         res.writeHead(500, { 'Content-Type': 'text/plain' });
-//         res.write('Error loading registration page');
-//         res.end();
-//       } else {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(data);
-//         res.end();
-//       }
-//     });
-//   } else {
-//     res.writeHead(404, { 'Content-Type': 'text/plain' });
-//     res.write('Page not found');
-//     res.end();
-//   }
-// });
-
-// server.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
-
-
-/*
-const fs = require("fs");
-
-fs.writeFile(
-  "sample.txt",
-  "Hello World. Welcome to Node.js File System module.",
-  (err) => {
-    if (err) throw err;
-    console.log("File created!");
-  }
-);
-
-fs.appendFile("sample.txt", " This is my updated content", (err) => {
-  if (err) throw err;
-  console.log("File updated!");
-});
-fs.readFile("sample.txt", (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
-
-fs.rename("sample.txt", "test.txt", (err) => {
-  if (err) throw err;
-  console.log("File name updated!");
-});
-
-fs.unlink("test.txt", (err) => {
-  if (err) throw err;
-  console.log("File test.txt deleted successfully!");
-});
-
-*/
-
-// const http=require("http");
-// const fs=require("fs");
-
-// const server=http.createServer((req,res) =>
-// {
-//   // fs.readFile("sample.txt" ,(err,data)=>
-//   // {
-//   //   res.end(data);
-//   // })
-
-//   const stream=fs.createReadStream("sample.txt");
-//   stream.pipe();
-  
-
-// });
-// server.listen(3000);
-
-
 const http = require("http");
 const fs = require("fs");
+const minimist = require("minimist");
+
+// Read the command line arguments
+const args = minimist(process.argv.slice(2));
+
+// Default port to 3000 if not provided
+const port = args.port || 3000;
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
+// Read home.html file
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
@@ -123,6 +20,7 @@ fs.readFile("home.html", (err, home) => {
   homeContent = home;
 });
 
+// Read project.html file
 fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err;
@@ -130,22 +28,32 @@ fs.readFile("project.html", (err, project) => {
   projectContent = project;
 });
 
+// Read registration.html file
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
 
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(3000);
-
+// Create the server
+http.createServer((request, response) => {
+  let url = request.url;
+  response.writeHead(200, { "Content-Type": "text/html" });
   
+  switch (url) {
+    case "/project":
+      response.write(projectContent);
+      break;
+    case "/registration":
+      response.write(registrationContent);
+      break;
+    default:
+      response.write(homeContent);
+      break;
+  }
+  
+  response.end();
+}).listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
